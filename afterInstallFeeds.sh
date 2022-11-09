5970538a0fbdba22fd224f0d5c65aea4c64848f8
NULLlnull <35243163+NULLlnull@users.noreply.github.com>
2022-11-09 09:18:40 +0800

    更新IP

--- afterInstallFeeds.sh
+++ afterInstallFeeds.sh
@@ -11,21 +11,20 @@
 #
 
 # Modify default IP
-#以后直接改File
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
+
+sed -i 's/192.168.1.1/172.35.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 #更换argon主题
 rm -rf openwrt/package/lean/luci-theme-argon
 rm -rf openwrt/package/feeds/kenzo/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon #argon-主题
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon #argon-主题
-
 git clone https://github.com/NULLlnull/luci-theme-rosy.git package/luci-theme-rosy
 
 #使用File大法，在编译好的时候先下载一份最新的GFW列表和大陆IP进去
 wget -O files/etc/ssrplus/china_ssr.txt https://ispip.clang.cn/all_cn.txt
 
 #修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
